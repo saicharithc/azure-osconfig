5de84cbf5d543913c14ec309656dec8c7d0980bb
saicharithc <saicharithc@microsoft.com>
2024-06-07 17:01:06 -0700

    nit

--- src/adapters/mc/asb/validationScripts/failChecks/fail_checks.sh
+++ src/adapters/mc/asb/validationScripts/failChecks/fail_checks.sh
@@ -14,7 +14,7 @@ chmod 777 ./*.sh
 
 sudo apt-get update
 sudo ./fail_file_permission_checks.sh
-sudo ./fail_network_config_checks.sh
+
 sudo ./fail_log_checks.sh
 sudo ./fail_partition_checks.sh
 sudo ./fail_sshd_config_checks.sh
@@ -22,8 +22,11 @@ sudo ./fail_password_checks.sh
 
 sudo ./install_old_packages.sh
 sudo ./enable_old_packages.sh
-sudo ./fail_misc.sh
+
 sudo ./fail_user_group_checks.sh
+
+sudo ./fail_misc.sh
+sudo ./fail_network_config_checks.sh
 
 # add a comment block
 # wget https://raw.githubusercontent.com/saicharithc/azure-osconfig/user/saicharithc/asmv2_validation_scripts/src/adapters/mc/asb/validationScripts/failChecks/fail_checks.sh
